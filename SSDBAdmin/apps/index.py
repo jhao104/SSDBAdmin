3e179c4aaf08733649328769b62d9f6c42f3708c
jhao104 <jingh@jccfc.com>
2018-08-27 10:53:49 +0800

    [update] 404 500 handle

--- SSDBAdmin/apps/index.py
+++ SSDBAdmin/apps/index.py
@@ -18,6 +18,16 @@ from SSDBAdmin.model.SSDBClient import getSAServer, SSDBClient
 from flask import render_template, request, make_response, redirect, url_for
 
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', error=error)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return redirect(url_for('index'))
+
+
 @app.route('/ssdbadmin/')
 def index():
     host, port = getSAServer(request)
